e5efcdc3379b83fa1b794f23a2b23726db6e4aab
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-29 12:57:14 -0300

    Building main layout

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,3 @@
 exports.homePage = async (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Cooking Blog - Homepage'}); // Changing the name of the title in layouts/main.js
 };
