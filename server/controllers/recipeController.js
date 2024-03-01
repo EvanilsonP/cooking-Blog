b6c6d8f7bf337ab7715e78baf67484726bd9bc06
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-01 10:38:01 -0300

    Rendering database recipes to the frontend

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,15 @@
+const Category = require('../models/Category');
+
 exports.homePage = async (req, res) => {
-    res.render('index', { title: 'Cooking Blog - Homepage'}); // Changing the name of the title in layouts/main.js
+    try {
+        // Once we want to show only 5 types of categories we use this const limited to 5
+        const limit = 5;
+        const categories = await Category.find({});
+        res.render('index', { title: 'Cooking Blog - Homepage', categories }); // Changing the name of the title in layouts/main.js // Displaying categories in home page
+
+    }  
+    catch (error) {
+        res.status(500).send({ message: error.message || 'An error occured.'});
+    }
 };
+
