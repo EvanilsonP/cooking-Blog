db4667fcf4251202cbf0c63a9279c9baa15a45ac
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-04 11:04:35 -0300

    Saving recipe to the database with image validation

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -118,14 +118,43 @@ exports.submitRecipe = async (req, res) => {
 // POST - SUBMIT RECIPE
 exports.submitRecipeOnPost = async (req, res) => {
     try {
-        const bodyData = req.body;
-        const recipe = new Recipe.create(bodyData);
+        let imageUploadFile;
+        let uploadPath;
+        let newImageName;
+        // If no file is uploaded
+        if(!req.files || Object.keys(req.files).length === 0){
+          console.log('No Files where uploaded.');
+        } else {
+    
+          imageUploadFile = req.files.image;
+          newImageName = Date.now() + imageUploadFile.name;
+    
+          uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+    
+          imageUploadFile.mv(uploadPath, function(err){
+            if(err) return res.satus(500).send(err);
+          })
+    
+        }
+
+        // Save recipe to the db
+        const newRecipe = new Recipe({
+            name: req.body.name,
+            description: req.body.description,
+            email: req.body.email,
+            ingredients: req.body.ingredients,
+            category: req.body.category,
+            image: newImageName
+          });
+          
+          await newRecipe.save();
 
         req.flash('infoSubmit', 'Recipe has been added. ');
         res.redirect('/submit-recipe');
     } 
     catch (error) {
         req.flash('infoErrors', error);
+        console.log(error);
         res.redirect('/submit-recipe');
     }
 };
